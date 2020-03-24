6c52ce31c90d009aa1f9c4456502cb65e3972e3b
vpmapelli <viniciusmapelli@gmail.com>
2020-03-24 13:36:52 -0300

    backend/IncidentController: GET list incidents (max 5 per page)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,7 @@ const routes = express.Router()
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
